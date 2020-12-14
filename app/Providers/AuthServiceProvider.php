e689574254389b5a9f12a65f30910889160d85cc
Estherbyamasu <estherbyamasufuraha@gmail.com>
2020-12-13 22:51:57 -0800

    updata in AuthServiceProvider

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -25,6 +25,16 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        Gate::define('manage-users',function($user){
+            return $user->hasAnyRole(['admin']);
+        });
+
+        Gate::define('edit-users',function($user){
+            return $user->hasAnyRole(['admin']);
+        });
+
+        Gate::define('delete-users',function($user){
+            return $user->isAdmin();
+        });
     }
 }
