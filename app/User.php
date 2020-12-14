89244001b754f2f2c3e6e9dd384b5304bc877ef5
Estherbyamasu <estherbyamasufuraha@gmail.com>
2020-12-13 22:50:02 -0800

    add model User.php

--- app/User.php
+++ app/User.php
@@ -36,4 +36,16 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function roles(){
+        return $this->BelongsToMany('App\Role');
+    }
+    public function isAdmin(){
+        return  $this->roles()->where('name','admin')->first();
+       }
+   
+       public function hasAnyRole(array $roles){
+           return  $this->roles()->whereIn('name',$roles)->first();
+       }
 }
+
