17648c9c131441d18c8f2abc37fbea8f43a55b64
Estherbyamasu <estherbyamasufuraha@gmail.com>
2020-12-13 22:56:01 -0800

    update in app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,7 +24,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    {{ config('app.name', 'Gestion caisse') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -64,6 +64,9 @@
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                         @csrf
                                     </form>
+                                    @can('manage-users')
+                                    <a  class="dropdowm-items" href="{{ route('admin.users.index') }}" >Liste des utilisateurs</a>
+                                    @endcan
                                 </div>
                             </li>
                         @endguest
