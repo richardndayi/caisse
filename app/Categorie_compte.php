fbbe847392087fd113f79a03583d01520d8fa914
richardndayi <ndaricad@gmail.com>
2020-12-14 22:20:28 -0800

    update data in CategoriesController.php

--- app/Categorie_compte.php
+++ app/Categorie_compte.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Categorie_compte extends Model
 {
-    //
+    public function comptes()
+    {
+        return $this->hasMany('App\Compte');
+    } 
 }
