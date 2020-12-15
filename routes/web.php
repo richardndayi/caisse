f64f979ccc9a8025d2d326cf72434266c77dcda9
habonimana aime parfait <aphabona@gmail.com>
2020-12-15 14:49:12 +0200

    add route

--- routes/web.php
+++ routes/web.php
@@ -31,3 +31,13 @@ Route::namespace('Admin')->prefix('admin')->name('admin.')->middleware('can:mana
 
 Route::get('home','HomeController@index');
 Route::resource('guichets','GuichetsController'); //Route pour un Guichet
+
+Route::get('caisses','CaissseController@index');
+Route::get('caisses/create','CaisseController@create');
+Route::get('caisses/edit/{compte}','CaisseController@edit');
+Route::get('caisses/{caisse}','CaisseController@show');
+Route::post('caisses','CaisseController@store');
+Route::post('caisses/destroy/{compte}','CaisseController@destroy');
+Route::put('caisses/{caisse}','CaisseController@update');
+Route::resource('caisses','CaisseController'); //Route pour une caisse
+
