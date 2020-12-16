15d0b5b17bbec8567e16e33a636545ec60f1ebbf
habonimana aime parfait <aphabona@gmail.com>
2020-12-16 14:20:14 +0200

    add ControllerCaisse

---
+++ app/Http/Controllers/CaisseDetailController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\CaisseDetail;
+use Illuminate\Http\Request;
+
+class CaisseDetailController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\CaisseDetail  $caisseDetail
+     * @return \Illuminate\Http\Response
+     */
+    public function show(CaisseDetail $caisseDetail)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\CaisseDetail  $caisseDetail
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(CaisseDetail $caisseDetail)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\CaisseDetail  $caisseDetail
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, CaisseDetail $caisseDetail)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\CaisseDetail  $caisseDetail
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(CaisseDetail $caisseDetail)
+    {
+        //
+    }
+}
