4497619d275c27b4ecb5d9aafe6d1565a5fd9073
habonimana aime parfait <aphabona@gmail.com>
2020-12-16 12:59:34 +0200

    update edit

--- resources/views/caisses/edit.blade.php
+++ resources/views/caisses/edit.blade.php
@@ -1,96 +1,84 @@
 @extends('templates.default')
 @section('content')
-<div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">
-    <div class="row">
-        <ol class="breadcrumb">
-            <li><a href="#">
-                <em class="fa fa-home"></em>
-            </a></li>
-            <li class="active">Caisse</li>
-        </ol>
-    </div><!--/.row-->
-
-
     <div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">
         <div class="row">
             <ol class="breadcrumb">
                 <li><a href="#">
                         <em class="fa fa-home"></em>
                     </a></li>
-                <li class="active">Mise à jour de la caisse</li>
+                <li class="active">Caisse</li>
             </ol>
         </div>
         <!--/.row-->
 
-        <div class="row">
-            <div class="col-lg-12">
-                <h1 class="page-header">Caisse</h1>
+
+        <div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">
+            <div class="row">
+                <ol class="breadcrumb">
+                    <li><a href="#">
+                            <em class="fa fa-home"></em>
+                        </a></li>
+                    <li class="active">Mise à jour de la caisse</li>
+                </ol>
             </div>
-        </div>
-        <!--/.row-->
+            <!--/.row-->
 
-        <div class="row">
-            <div class="col-lg-12">
-                <div class="panel panel-default">
-                    <div class="panel-heading">Modifier la caisse</div>
-                    <div class="panel-body">
-                        <div class="col-md-12">
-                            <form role="form" action="{{ route('caisses.update', $caisse->id) }}" method="post">
-                                @csrf
-                                
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="page-header">Caisse</h1>
+                </div>
+            </div>
+            <!--/.row-->
 
-                                <div class="form-row">
-                                    <div class="form-group col-md-6 ">
-                                        <label for="description"><strong> Numero de la Caisse</strong></label>
-                                        <textarea name="numero_caisse" id="numero_caisse" value="" rows="5" cols="100"
-                                            class="form-control" class="@error('numero_caisse') is-danger @enderror"
-                                            placeholder="Tapez le numero de la caisse ici" aria-describedby="helpId">
-                                        {{ $caisse->numero_caisse }}
-                                        </textarea>
-                                        @error('numero_caisse')
-                                            <div class="alert alert-danger">{{ $message }}</div>
-                                        @enderror
-                                    </div>
-                                </div>
-
-       
-                                <div class="form-row">
-                                    <div class="form-group col-md-6 ">
-                                        <label for="description"><strong> Solde de la caisse</strong></label>
-                                        <textarea name="solde" id="solde" value="" rows="5" cols="100"
-                                            class="form-control" class="@error('solde') is-danger @enderror"
-                                            placeholder="Tapez le solde de la caisse" aria-describedby="helpId">
-                                        {{ $caisse->solde}}
-                                        </textarea>
-                                        @error('solde')
-                                            <div class="alert alert-danger">{{ $message }}</div>
-                                        @enderror
-                                    </div>
-                                </div>
-
-
-                                <div class="form-group">
-                                    <label>Numero du Guichet</label> 
-                                    <select name="guichet_id" style="width:300px; height:35px ;background:gray; color:white" id="" class="form-control"
-                                    class="@error('nomero_guichet') is-danger @enderror"> 
-                                    <option value="" >Select numero Guichet</option>
-                                    @foreach($$guichets as $guichet)
-                                    <option value="{{$guichet->id}}">{{$guichet->nomero_guichet}}</option>
-                                    @endforeach
-                                    @error('guichet_id')
-                                    <div class="alert alert-danger">{{$message}}</div>
-                                    @enderror
-                                    </select>
-                                </div>
-
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">Modifier la caisse</div>
+                        <div class="panel-body">
+                            <div class="col-md-12">
+                                <form role="form" action="{{ route('caisses.update', $caisse->id) }}" method="POST">
+                                    @method('PUT')
+                                    @csrf
+                                    <label for="description"><strong> Numero de la Caisse</strong></label>
+									
+                                    <input type="text" style="width:300px; height:35px ;background:rgb(26, 29, 29); color:white" 
+                                    name="numero_caisse" class="form-control" placeholder="" value="{{$caisse->numero_caisse}}">
                                  
-                                </div>
+                                    <label for="description"><strong> Solde</strong></label>
+									
+                                    <input type="text" style="width:300px; height:35px ;background:rgb(24, 21, 21); color:white" 
+                                    name="solde" class="form-control" placeholder="" value="{{$caisse->solde}}">
+
+                                    <div class="form-group">
+                                        <div class="form-group">
+                                            <label>Numero du Guichet</label>
+                                            <select name="guichet_id"
+                                                style="width:300px; height:35px ;background:rgb(17, 17, 17); color:white" id=""
+                                                class="form-control" class="@error('nom_guichet') is-danger @enderror">
+                                                <option value="">Select numero Guichet</option>
+                                                @foreach ($guichets as $guichet)
+                                                    <option value="{{ $guichet->id }}">{{ $guichet->nomero_guichet }}
+                                                    </option>
+                                                @endforeach
+                                                @error('guichet_id')
+                                                    <div class="alert alert-danger">{{ $message }}</div>
+                                                @enderror
+                                            </select>
+                                        </div>
+
+                                    </div>
+                                    <button type="submit"
+                                onclick="return confirm('Voulez vous modifier la verification qui a ete faite ?')"
+                                class="btn btn-warning">Modifier</button>
+                            <button type="reset" class="btn btn-primary">Reset</button>
+                                </form>
+                            </div>
+                            
+
                         </div>
-                        </form>
+
                     </div>
-                    <button type="submit"onclick="return confirm('Voulez vous modifier la verification qui a ete faite ?')"
-                        class="btn btn-warning">Modifier</button>
-                    <button type="reset" class="btn btn-primary">Reset</button>
+
                 </div>
             </div><!-- /.panel-->
         </div><!-- /.panel-->
